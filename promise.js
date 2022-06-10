d537e8657fefdd0dc6411de5aa03bdee748f977f
Yadhies1989 <yadi.sepriadi@gmail.com>
2022-06-10 22:42:59 +0700

    commit chalenge 7

--- promise.js
+++ promise.js
@@ -1,7 +1,29 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+    try {
+
+      let totalData = 0;
+
+      const teater1 = await promiseTheaterIXX();
+      const teater2 = await promiseTheaterVGC();
+
+      const dataGabungan = teater1.concat(teater2)
+
+      dataGabungan.forEach(element => {
+        if (element.hasil === emosi) {
+        totalData++;
+        }
+      });
+
+      return totalData;
+
+    } catch (err) {
+      console.log(err);
+    }
+
+}
 
 module.exports = {
   promiseOutput,
